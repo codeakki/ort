b2c8aaf9978b0ff352f4782922df9b0b441853ba
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-03-08 10:25:27 +0100

    CycloneDxReporter: Update to generate schema version 1.2 BOMs
    See https://github.com/CycloneDX/specification/releases/tag/1.2.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- reporter/src/funTest/kotlin/reporters/CycloneDxReporterFunTest.kt
+++ reporter/src/funTest/kotlin/reporters/CycloneDxReporterFunTest.kt
@@ -37,10 +37,10 @@ import org.ossreviewtoolkit.utils.ORT_NAME
 class CycloneDxReporterFunTest : StringSpec({
     val options = mapOf("single.bom" to "true")
 
-    "A generated BOM should be valid according to schema version 1.1" {
+    "A generated BOM should be valid according to schema version 1.2" {
         val outputDir = createTempDirectory("$ORT_NAME-${javaClass.simpleName}").toFile().apply { deleteOnExit() }
         val bomFile = CycloneDxReporter().generateReport(ReporterInput(ORT_RESULT), outputDir, options).single()
 
-        XmlParser().validate(bomFile, CycloneDxSchema.Version.VERSION_11) should beEmpty()
+        XmlParser().validate(bomFile, CycloneDxSchema.Version.VERSION_12) should beEmpty()
     }
 })
